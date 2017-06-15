963cee34a5afba12db8ca49a4c4aa6de12347f8a
prastut <kr.prastut@gmail.com>
2017-06-15 21:36:55 +0530

    Checking image payload

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 'use strict'
+var data = {
+    "messages": [{
+        "attachment": {
+            "type": "image",
+            "payload": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/1200px-Banana-Single.jpg"
+            }
+        }
+    }]
+};
+
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -8,28 +19,27 @@ const app = express()
 app.set('port', (process.env.PORT || 443))
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Process application/json
 app.use(bodyParser.json())
 
 // Index route
-app.get('/', function (req, res) {
-	var jsonResponse = [];
-    jsonResponse.push({ "text": "Paul Scholes" });
-	res.send(jsonResponse);
+app.get('/', function(req, res) {
+    var jsonResponse = [];
+    jsonResponse.push(data);
+    res.send(jsonResponse);
 })
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token')
+app.get('/webhook/', function(req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
 })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
+    console.log('running on port', app.get('port'))
 })
-
