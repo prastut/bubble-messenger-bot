bb314dfec4aac62a8a62242130bff5a8ef655bf2
prastut <kr.prastut@gmail.com>
2017-06-19 20:07:41 +0530

    image_webshot

--- routes/get-team-data.js
+++ routes/get-team-data.js
@@ -25,70 +25,61 @@ router.get('/', function(req, res, next) {
                     return console.error('upload failed:', err);
                 }
 
-                res.send(teamData(data, flag));
+                var channel = Object.keys(data)[1 - Object.keys(data).indexOf('instance_id')];
+                var keysSorted = Object.keys(data[channel]).map(Number).sort();
+                var maxKey = keysSorted[keysSorted.length - 1];
+
+                // console.log(data);
+
+                var neg = data[channel][maxKey].neg;
+                var pos = data[channel][maxKey].pos;
+
+                var screenshotUrl = helper.ip + "screenshot" +
+                    "?title=" + channel +
+                    "&channel=" + channel +
+                    "&flag=" + flag +
+                    "&neg=" + neg +
+                    "&pos=" + pos;
+
+
+                var savePath = path.join('/root/bot/public', 'img', 'screenshot', channel + '-screenshot.jpeg');
+
+                console.log(savePath);
+
+                var image_url = path.join(helper.ip, 'img', 'screenshot', channel + '-screenshot.jpeg');
+
+                console.log(image_url);
+
+                webshot(screenshotUrl, savePath, helper.optionsPhone, function(err) {
+                    console.log(err);
+
+                    var payload = {
+                        "messages": [{
+                            "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "generic",
+                                    "elements": [{
+                                        "title": "2",
+                                        "image_url": 'http://www.indiacelebrating.com/wp-content/uploads/indian-Flag.jpg',
+                                        "buttons": [{
+                                            "type": "web_url",
+                                            "url": "http:/139.59.25.186/img/screenshot/india-screenshot.jpeg",
+                                            "title": "View Sentiment Analysis"
+                                        }]
+                                    }]
+                                }
+                            }
+                        }]
+                    };
+
+                    res.send(payload);
+                    // return payload;
+                });
+
             });
 
     }
 });
-
-
-function teamData(data, flag) {
-
-    var channel = Object.keys(data)[1 - Object.keys(data).indexOf('instance_id')];
-    var keysSorted = Object.keys(data[channel]).map(Number).sort();
-    var maxKey = keysSorted[keysSorted.length - 1];
-
-    console.log(data);
-
-    var neg = data[channel][maxKey].neg;
-    var pos = data[channel][maxKey].pos;
-
-    var screenshotUrl = helper.ip + "screenshot" +
-        "?title=" + channel +
-        "&channel=" + channel +
-        "&flag=" + flag +
-        "&neg=" + neg +
-        "&pos=" + pos;
-
-
-    var savePath = path.join('/root/bot/public', 'img', 'screenshot', channel + '-screenshot.jpeg');
-
-    console.log(savePath);
-
-    var image_url = path.join(helper.ip, 'img', 'screenshot', channel + '-screenshot.jpeg');
-
-    console.log(image_url);
-
-    webshot(screenshotUrl, savePath, helper.optionsPhone, function(err) {
-        console.log(err);
-
-        var payload = {
-            "messages": [{
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "2",
-                            "image_url": image_url,
-                            "buttons": [{
-                                "type": "web_url",
-                                "url": "http:/139.59.25.186/img/screenshot/india-screenshot.jpeg",
-                                "title": "View Sentiment Analysis"
-                            }]
-                        }]
-                    }
-                }
-            }]
-        };
-
-
-        return payload;
-    });
-
-
-
-}
-
 
 module.exports = router;
