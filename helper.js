e370f6a9421c8647fd20463399f2d19245ad1e13
prastut <kr.prastut@gmail.com>
2017-06-19 20:45:02 +0530

    image_webshot

--- helper.js
+++ helper.js
@@ -27,7 +27,7 @@ module.exports = {
         userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)' +
             ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g',
 
-        renderDelay: 1000
+        renderDelay: 10000
     }
 
 };
