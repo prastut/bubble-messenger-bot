3918f824889ebb08cedeb0f840b45c4f02c09e81
prastut <kr.prastut@gmail.com>
2017-06-21 17:12:20 +0530

    routes

--- routes/quick-replies.js
+++ routes/quick-replies.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 const helper = require("../helper.js");
 
 
