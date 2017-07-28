142ea90fd1949a1ecb9e59a7263b4e41d3fe1cb9
prastut <kr.prastut@gmail.com>
2017-07-28 21:08:04 +0530

    players bot done

--- routes/bot/step-3/quick-replies.js
+++ routes/bot/step-3/quick-replies.js
@@ -16,124 +16,60 @@ router.get('/', function(req, res, next) {
 
             var payload,
                 elements = [],
-                instance_id = data.instance_id,
-                i,
-                player,
-                name,
-                web_url,
-                quick_replies = helper.quickReplies(match_id, type);
+                quick_replies = helper.quickReplies(match_id, type),
+                requestedPlayers,
+                i;
+
 
             if (type == "trending") {
-
-                var trendingPlayers = data.trending;
-
-
-                async.map([...trendingPlayers], fetchplayerDetails, function(err, results) {
-                    if (err) {
-
-                    } else {
-
-                        for (i in results) {
-
-                            var player = results[i];
-
-                            elements.push({
-                                "title": player.pretty_name,
-                                "image_url": player.image_url,
-                                "subtitle": "Tweet Count: " + player.tweets_count,
-                                "buttons": [{
-                                    "type": "web_url",
-                                    "url": player.webview,
-                                    "title": "View More",
-                                    "webview_height_ratio": "tall",
-                                }]
-                            });
-
-                        }
-
-                        payload = {
-                            "messages": [{
-                                "attachment": {
-                                    "type": "template",
-                                    "payload": {
-                                        "template_type": "generic",
-                                        "elements": elements
-                                    }
-                                }
-                            }, {
-                                "text": "In addition you can do the following as well:",
-                                "quick_replies": quick_replies
-                            }]
-                        };
-
-                        res.send(payload);
-                    }
-                });
-
-
-            } else if (type == "herozero") {
-
-                var hero = data.heros;
-                var zero = data.zeros;
-
-                for (i in hero) {
-
-                    player = Object.keys(hero[i])[0];
-                    name = hero[i][player].name;
-                    webview_url = helper.webviewURL(player, name, instance_id);
-
-                    elements.push({
-                        "title": hero[i][player].name,
-                        "image_url": hero[i][player].img_url,
-                        "subtitle": "Tweet Count: " + hero[i][player].total_tweets,
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": webview_url,
-                            "title": "View More",
-                            "webview_height_ratio": "tall",
-                        }]
-                    });
-                }
-
-                for (i in zero) {
-
-                    player = Object.keys(zero[i])[0];
-                    name = zero[i][player].name;
-                    webview_url = helper.webviewURL(player, name, instance_id);
-
-                    elements.push({
-                        "title": zero[i][player].name,
-                        "image_url": zero[i][player].img_url,
-                        "subtitle": "Tweet Count: " + zero[i][player].total_tweets,
-                        "buttons": [{
-                            "type": "web_url",
-                            "url": webview_url,
-                            "title": "View More",
-                            "webview_height_ratio": "tall",
-
-                        }]
-                    });
-                }
-
-                payload = {
-                    "messages": [{
-                        "attachment": {
-                            "type": "template",
-                            "payload": {
-                                "template_type": "list",
-                                "top_element_style": "compact",
-                                "elements": elements
-                            }
-                        }
-                    }, {
-                        "text": "In addition you can do the following as well:",
-                        "quick_replies": quick_replies
-                    }]
-                };
-
+                requestedPlayers = data.trending;
+            } else if (type == "hero") {
+                requestedPlayers = data.heros;
+            } else if (type == "zero") {
+                requestedPlayers = data.zeros;
             }
 
-            // res.send(payload);
+            async.map([...requestedPlayers], fetchplayerDetails, function(err, results) {
+                if (err) {
+
+                } else {
+
+                    for (i in results) {
+
+                        var player = results[i];
+
+                        elements.push({
+                            "title": player.pretty_name,
+                            "image_url": player.image_url,
+                            "subtitle": "Tweet Count: " + player.tweets_count,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": player.webview,
+                                "title": "View More",
+                                "webview_height_ratio": "tall",
+                            }]
+                        });
+
+                    }
+
+                    payload = {
+                        "messages": [{
+                            "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "generic",
+                                    "elements": elements
+                                }
+                            }
+                        }, {
+                            "text": "In addition you can do the following as well:",
+                            "quick_replies": quick_replies
+                        }]
+                    };
+
+                    res.send(payload);
+                }
+            });
         });
 
 
